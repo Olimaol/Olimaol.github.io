f26fd9ebdfc7fc2c9c9db7e90f6317182779ccf8
olmai <oliver.maith@informatik.tu-chemnitz.de>
2021-09-02 22:15:40 +0200

    new function

--- function.js
+++ function.js
@@ -1,3 +1,12 @@
 function print_something() {
     alert("Hallo Welt!");
 }
+
+function check() {
+    const cb = document.querySelector('#id1');
+    if (cb.checked == true) {
+        cb.checked = false;
+    } else {
+        cb.checked = true;
+    }
+}
