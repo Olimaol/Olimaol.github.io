68caee04514cb96f67586e3f40bffc3bf8b480e5
olmai <oliver.maith@informatik.tu-chemnitz.de>
2021-09-03 14:53:35 +0200

    added firebase database

--- function.js
+++ function.js
@@ -10,3 +10,26 @@ function check() {
         cb.checked = true;
     }
 }
+
+function lock_cb() {
+    const cb = document.querySelector('#id1');
+    if (cb.disabled == true) {
+        cb.disabled = false;
+    } else {
+        cb.disabled = true;
+    }
+}
+
+function check_and_lock() {
+    const cb = document.querySelector('#id1');
+    cb.disabled=true;
+    cb.checked=true;
+}
+
+function saveToFirebase() {
+    var emailObject = {
+        email: "email"
+    };
+
+    firebase.database().ref('subscription-entries').push().set(emailObject)
+}
