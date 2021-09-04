a059ca73e33930b6062b9d427ba101ad83e96494
olmai <oliver.maith@informatik.tu-chemnitz.de>
2021-09-04 10:35:30 +0200

    added: send checkbox statuses to firebase

--- function.js
+++ function.js
@@ -33,3 +33,33 @@ function saveToFirebase() {
 
     firebase.database().ref('subscription-entries').push().set(emailObject)
 }
+
+function write_to_id(id, checkstatus) {
+    firebase.database().ref('checkboxes/' + id).set({
+        checked: checkstatus
+    });
+}
+
+function lock_element(element, index, array) {
+    element.disabled=true;
+}
+
+function send_check_status(element, index, array) {
+    id=element.id;
+    checkstatus=element.checked;
+    firebase.database().ref('checkboxes/' + id).set({
+        checked: checkstatus
+    });
+}
+
+function send_checks(params) {
+    // lock all checkboxes which are checked
+    const checked_cb_list = document.querySelectorAll('input[type="checkbox"]:checked');
+    checked_cb_list.forEach(lock_element);
+
+    // send for each cb the checked status to firebase
+    const cb_list = document.querySelectorAll('input[type="checkbox"]');
+    cb_list.forEach(send_check_status);
+
+
+}
